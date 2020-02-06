80dc31d8a1b9eaea9324da9eb4041f020ae13b58
Raphael Marinier <raphaelm@google.com>
2020-02-06 16:56:09 +0100

    Added names to Aggregators to make debugging easier.
    PiperOrigin-RevId: 293592158

--- agents/vtrace/learner.py
+++ agents/vtrace/learner.py
@@ -298,14 +298,16 @@ def learner_loop(create_env_fn, create_agent_fn, create_optimizer_fn):
       tf.TensorSpec([], tf.float32, 'episode_returns'),
       tf.TensorSpec([], tf.float32, 'episode_raw_returns'),
   )
-  actor_infos = utils.Aggregator(FLAGS.num_actors, info_specs)
+  actor_infos = utils.Aggregator(FLAGS.num_actors, info_specs, 'actor_infos')
 
   # First agent state in an unroll.
-  first_agent_states = utils.Aggregator(FLAGS.num_actors, agent_state_specs)
+  first_agent_states = utils.Aggregator(
+      FLAGS.num_actors, agent_state_specs, 'first_agent_states')
 
   # Current agent state and action.
-  agent_states = utils.Aggregator(FLAGS.num_actors, agent_state_specs)
-  actions = utils.Aggregator(FLAGS.num_actors, action_specs)
+  agent_states = utils.Aggregator(
+      FLAGS.num_actors, agent_state_specs, 'agent_states')
+  actions = utils.Aggregator(FLAGS.num_actors, action_specs, 'actions')
 
   unroll_specs = Unroll(agent_state_specs, *store.unroll_specs)
   unroll_queue = utils.StructuredFIFOQueue(1, unroll_specs)

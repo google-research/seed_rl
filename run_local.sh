e8e8811ca65baa3532a3313c79886f9e535963f0
Lasse Espeholt <lespeholt@google.com>
2020-03-23 17:06:13 +0100

    Internal
    PiperOrigin-RevId: 302445978

--- run_local.sh
+++ run_local.sh
@@ -24,7 +24,6 @@ AGENTS="r2d2|vtrace"
 [ "$#" -ne 0 ] || die "Usage: run_local.sh [$ENVIRONMENTS] [$AGENTS] [Num. actors]"
 echo $1 | grep -E -q $ENVIRONMENTS || die "Supported games: $ENVIRONMENTS"
 echo $2 | grep -E -q $AGENTS || die "Supported agents: $AGENTS"
-echo $3 | grep -E -q "^((0|([1-9][0-9]*))|(0x[0-9a-fA-F]+))$" || die "Number of actors should be a non-negative integer without leading zeros"
 export ENVIRONMENT=$1
 export AGENT=$2
 export NUM_ACTORS=$3
